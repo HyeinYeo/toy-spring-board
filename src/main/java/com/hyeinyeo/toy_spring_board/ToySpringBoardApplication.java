dd9a715974c3fd8db1df454aa6872a0bd62077bc
HyeinYeo <s202002086@hufs.ac.kr>
2025-02-11 02:16:15 +0900

    initial commit

---
+++ src/main/java/com/hyeinyeo/toy_spring_board/ToySpringBoardApplication.java
@@ -0,0 +1,13 @@
+package com.hyeinyeo.toy_spring_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToySpringBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToySpringBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyeinyeo/toy_spring_board/ToySpringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeinyeo.toy_spring_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToySpringBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

53c17df3c3ef9923e85ca27971698360e8320917
Gamza <100903181+HyeinYeo@users.noreply.github.com>
2025-03-05 20:35:03 +0900

    feat: add redirection to articles page for logged-in users on home route (#16)
    - 홈(/) 호출 시
      - 로그인 한 사용자인 경우, 게시글 목록 페이지로 리다이렉션
      - 로그인 하지 않은 사용자의 경우, 홈으로 라우팅

--- src/main/java/com/hyeinyeo/toy_spring_board/controller/HomeController.java
+++ src/main/java/com/hyeinyeo/toy_spring_board/controller/HomeController.java
@@ -1,12 +1,20 @@
 package com.hyeinyeo.toy_spring_board.controller;
 
+import com.hyeinyeo.toy_spring_board.constants.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String home(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
+            return "home";
+        }
+
+        return "redirect:/articles";
     }
 }
